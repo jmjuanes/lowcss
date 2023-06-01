a44cefcaadd5f9e5c92f529594b9df90313f62f5
Josemi <josemijuanes@gmail.com>
2023-06-01 19:38:17 +0200

    Update documentation

--- scripts/mdx.js
+++ scripts/mdx.js
@@ -5,7 +5,6 @@ const {renderToStaticMarkup} = require("react-dom/server");
 const runtime = require("react/jsx-runtime");
 const matter = require("gray-matter");
 const hljs = require("highlight.js/lib/common");
-const mochicons = require("@mochicons/node");
 
 const pkg = require("../package.json");
 const lowData = require("../dist/low.json");
@@ -29,20 +28,30 @@ const importPackages = () => {
     ]);
 };
 
+const ICONS = {
+    "arrow-right": "M4 12L20 12M14 6L20 12L14 18",
+    "bars": "M4 6L20 6M4 12L20 12M4 18L20 18",
+    "bolt": "M14 3L5 14L11 14L10 21L19 10L13 10L14 3Z",
+    "mobile": "M8 3C6 3 6 5 6 5L6 19C6 19 6 21 8 21L16 21C18 21 18 19 18 19L18 5C18 5 18 3 16 3L8 3ZM12 17L12 17M10 4L14 4",
+    "palette": "M6 20C4 20 4 18 4 18L4 6C4 6 4 4 6 4L10 4C12 4 12 6 12 6L12 18C12 18 12 20 10 20L6 20ZM10 20L18 20C20 20 20 18 20 18L20 15C20 15 20 13 18 13L17 13M12 8L14 6C15 5 16 5 17 6L19 8C20 9 20 10 19 11L12 18M8 16L8 16",
+};
+
 const Icon = props => (
     <svg xmlns="http-//www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24">
-        <path d={mochicons[props.icon].path} fill="none" strokeWidth="2" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" />
+        <path d={ICONS[props.icon]} fill="none" strokeWidth="2" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" />
     </svg>
 );
 
 const pageComponents = {
     "h1": props => <h1 className="mt-8 mb-4 text-gray-800 text-2xl font-bold">{props.children}</h1>,
     "h2": props => <h2 className="mt-8 mb-4 text-gray-800 text-xl font-bold">{props.children}</h2>,
-    "p": props => <p className="mt-6 mb-6 text-justify">{props.children}</p>,
+    "p": props => <p className="mt-6 mb-6">{props.children}</p>,
+    "ul": props => <ul style={{listStylePosition: "inside"}}>{props.children}</ul>,
+    "ol": props => <ol style={{listStylePosition: "inside"}}>{props.children}</ol>,
     "li": props => <li className="mb-3">{props.children}</li>,
     "code": props => <code className="font-mono text-sm">{props.children}</code>,
     "pre": props => {
-        const className = "p-4 rounded-md bg-white border border-solid border-gray-300 overflow-auto mb-8";
+        const className = "p-4 rounded-md bg-gray-100 border border-solid border-gray-300 overflow-auto mb-8";
         const items = React.Children.toArray(props.children);
         const code = items[0].props.children;
         const language = (items[0].props.className || "").replace("language-", "");
@@ -88,6 +97,12 @@ const MenuLink = props => (
     </a>
 );
 
+const NavbarLink = props => (
+    <a href={props.href} className="font-medium text-gray-600 hover:text-gray-800 no-underline">
+        {props.text}
+    </a>
+);
+
 const PageNavigation = props => {
     const prevPage = props.pages.find(p => p.fileName === props.page.data.prev);
     const nextPage = props.pages.find(p => p.fileName === props.page.data.next);
@@ -118,14 +133,14 @@ const HomeLayout = props => (
     <div className="w-full">
         {props.page.element}
         {props.page.data.features && (
-            <div className="w-full grid gap-8 md:grid-cols-2 grid-cols-1">
+            <div className="w-full grid gap-8 md:grid-cols-3 grid-cols-1">
                 {props.page.data.features.map(feature => (
                     <div className="bg-gray-100 rounded-md p-8" key={feature.title}>
                         <div className="mb-4 text-4xl">
                             {React.createElement(pageComponents.Icon, {icon: feature.icon})}
                         </div>
-                        <div className="font-bold text-xl mb-4">{feature.title}</div>
-                        <div className="">{feature.description}</div>
+                        <div className="font-bold text-lg mb-4">{feature.title}</div>
+                        <div className="text-sm">{feature.description}</div>
                     </div>
                 ))}
             </div>
@@ -144,11 +159,10 @@ const DocsLayout = props => (
                     <MenuLink href="customize.html" text="Customize" />
                 </MenuSection>
                 <MenuSection>
-                    <MenuGroup text="Extra Styles" />
+                    <MenuGroup text="Additional Styles" />
                     <MenuLink href="reset.html" text="Reset CSS" />
-                    <MenuLink href="keyframes.html" text="Keyframes" />
+                    {/* <MenuLink href="keyframes.html" text="Keyframes" /> */}
                 </MenuSection>
-
                 {Object.entries(utilitiesMap).map(section => (
                     <MenuSection key={section[0]}>
                         <MenuGroup text={section[0]} />
@@ -190,20 +204,29 @@ const PageWrapper = props => (
         </head>
         <body className="bg-white m-0 p-0 font-inter text-gray-700 leading-normal">
             {/* Header */}
-            <div className="border-b-1 border-solid border-gray-300">
+            <div className="border-b-1 border-gray-300 relative">
                 <div className="w-full maxw-7xl h-20 px-6 mx-auto flex items-center justify-between">
                     <a href="./index.html" className="flex items-center gap-1 text-gray-800 no-underline">
                         <div className="font-black font-crimson text-2xl tracking-tight">
                             <span>lowCSS.</span>
                         </div>
                     </a>
-                    <div className="flex gap-6 items-center">
-                        <a href="./installation.html" className="font-medium text-gray-600 hover:text-gray-800 no-underline">Installation</a>
-                        <a href="./utilities.html" className="font-medium text-gray-600 hover:text-gray-800 no-underline">Utilities</a>
-                        <div className="w-px h-6 bg-gray-300" />
-                        <a href={pkg.repository} className="font-medium text-gray-600 hover:text-gray-800 no-underline">
-                            <img src="https://www.josemi.xyz/github.svg" />
-                        </a>
+                    <div className="group" tabIndex="0">
+                        <div className="flex sm:hidden text-xl p-2 border border-gray-300 rounded-md">
+                            <Icon icon="bars" />
+                        </div>
+                        <div className="absolute sm:initial w-full sm:w-auto top-full left-0 bg-white p-8 sm:p-0 hidden sm:block group-focus-within:block">
+                            <div className="flex flex-col sm:flex-row gap-6 items-center">
+                                <NavbarLink href="./installation.html" text="Installation" />
+                                <NavbarLink href="./syntax.html" text="Syntax" />
+                                <NavbarLink href="./customize.html" text="Customize" />
+                                <NavbarLink href="./utilities.html" text="Utilities" />
+                                <div className="w-px h-6 bg-gray-300 hidden sm:block" />
+                                <a href={pkg.repository} className="no-underline o-70 hover:o-100">
+                                    <img className="w-6 h-6" src="./github.svg" />
+                                </a>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -218,7 +241,7 @@ const PageWrapper = props => (
             </div>
             {/* Footer */}
             <div className="w-full maxw-7xl mx-auto px-6 pt-10 pb-20">
-                <div className="mb-12 border border-dashed border-gray-200" />
+                <div className="mb-12 border-t-1 border-gray-300" />
                 <div className="text-center text-sm">
                     Designed by <a href="https://josemi.xyz" className="no-underline text-gray-800 hover:text-gray-700 font-bold">Josemi</a>. 
                     Released under the <b>MIT</b> License.
