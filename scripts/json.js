d5b73f66195b02f589312f7293fa98ba7fd9ed20
Josemi <josemijuanes@gmail.com>
2023-06-07 18:20:07 +0200

    Fix script for parsing output json

--- scripts/json.js
+++ scripts/json.js
@@ -11,11 +11,8 @@ const run = () => {
     process.stdin.on("end", () => {
         const content = JSON.parse(data.join(""));
         content.version = pkg.version;
-        content.utilities = content.utilities.map(utility => {
-            // Fix pseudos and properties fields of json
-            // If those lists only contains one item, it is saved as a string instead of as an array
-            utility.properties = [utility.properties].flat();
-            utility.pseudos = (Array.isArray(utility.pseudos) ? utility.pseudos : [utility.pseudos]).flat();
+        Object.keys(content.utilities).forEach(key => {
+            const utility = content.utilities[key];
             // Fix values: convert arrays to joined strings
             Object.keys(utility.values).forEach(key => {
                 const value = utility.values[key];
@@ -24,7 +21,6 @@ const run = () => {
                     utility.values[key] = value.join(delimiter);
                 }
             });
-            return utility;
         });
         // Print output json
         process.stdout.write(printJson(content) + "\n");
