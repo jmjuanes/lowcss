95923748c4bf7676ad112a35823e9b569a69e326
Josemi Juanes <5751201+jmjuanes@users.noreply.github.com>
2024-04-07 15:57:23 +0000

    Include markdown pages as partials

--- scripts/site.js
+++ scripts/site.js
@@ -1,5 +1,6 @@
 const fs = require("node:fs/promises");
 const path = require("node:path");
+const marked = require("marked");
 const pkg = require("../package.json");
 
 const getData = () => {
@@ -9,6 +10,14 @@ const getData = () => {
             title: pkg.title,
             description: pkg.description,
             repository: pkg.repository,
+            sidenav: Object.values({
+                gettingStarted: {
+                    title: "Getting Started",
+                    items: [
+                        {title: "Introduction", link: "#introduction"},
+                    ],
+                },
+            }),
         },
         features: [
             {
@@ -30,10 +39,26 @@ const getData = () => {
     };
 };
 
+// @description get partials
+const getPartials = async () => {
+    const partials = {};
+    const docsPath = path.join(process.cwd(), "docs");
+    const markdownFiles = await fs.readdir(docsPath, "utf8");
+    for (let i = 0; i < markdownFiles.length; i++) {
+        const file = markdownFiles[i];
+        if (path.extname(file) === ".md") {
+            const content = await fs.readFile(path.join(docsPath, file), "utf8");
+            partials[path.basename(file, ".md")] = marked.parse(content);
+        }
+    }
+    return partials;
+};
+
 const build = async () => {
     const m = (await import("mikel")).default;
     const template = await fs.readFile(path.join(process.cwd(), "index.mustache"), "utf8");
-    const result = m(template, getData());
+    const partials = await getPartials();
+    const result = m(template, getData(), {partials});
     await fs.writeFile(path.join(process.cwd(), "public", "index.html"), result, "utf8");
 };
 
