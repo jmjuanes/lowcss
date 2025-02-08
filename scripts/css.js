beb0f9ba3ac0c8a7a391dcea190ec1b149fedbd7
Josemi Juanes <5751201+jmjuanes@users.noreply.github.com>
2025-02-09 00:30:00 +0100

    chore: change build to generate low modules (#48)

---
+++ scripts/css.js
@@ -0,0 +1,63 @@
+const fs = require("node:fs");
+const sass = require("sass");
+const autoprefixer = require("autoprefixer");
+const postcss = require("postcss");
+const {minify} = require("csso");
+
+// output modules to compile
+const outputModules = Object.values({
+    "low.css": {
+        output: "low.css",
+        enabledModules: [
+            "root",
+            "reset",
+            "starter",
+            "markup",
+            "utilities",
+        ],
+    },
+    "low.themes.css": {
+        output: "low.themes.css",
+        enabledModules: [
+            "themes",
+        ],
+    },
+});
+
+// @description build scss
+const buildScss = code => {
+    const {css} = sass.compileString(code, {
+        loadPaths: [
+            process.cwd(),
+        ],
+    });
+    return postcss([autoprefixer]).process(css).then(result => {
+        result.warnings().forEach(warn => {
+            console.warn(warn.toString());
+        });
+        return minify(result.css, {
+            sourceMap: false,
+        });
+    });
+};
+
+const build = () => {
+    const template = fs.readFileSync("main.scss", "utf8");
+    console.log(`[build:css] generating ${outputModules.length} modules...`);
+    // generate each module
+    const allPromises = outputModules.map(item => {
+        const enabledModulesStr = JSON.stringify(item.enabledModules);
+        const code = template.replace("$enabled-modules: ();", `$enabled-modules: (${enabledModulesStr});`);
+        return buildScss(code).then(result => {
+            fs.writeFileSync(item.output, result.css);
+            console.log(`[build:css] saved '${item.output}'`);
+        });
+    });
+    // when all promises are finised
+    Promise.all(allPromises).then(() => {
+        console.log(`[build:css] build finished`);
+    });
+};
+
+// Build lowcss
+build();
