aa75658d14e76d13f283d0f7e520e07abb487eb4
Josemi <josemijuanes@gmail.com>
2025-06-03 18:41:01 +0200

    Add release script

---
+++ scripts/release.js
@@ -0,0 +1,21 @@
+import * as fs from "node:fs";
+import * as path from "node:path";
+import pkg from "../package.json" with {type: "json"};
+
+// tiny helper to read and write a JSON file
+const readJson = file => {
+    return JSON.parse(fs.readFileSync(file, "utf8"));
+};
+const writeJson = (file, data) => {
+    return fs.writeFileSync(file, JSON.stringify(data, null, "    "), "utf8");
+};
+
+// Fix the version in each package.json of the addons folder
+const packagesDir = path.join(process.cwd(), "addons");
+fs.readdirSync(packagesDir).forEach(dir => {
+    const packageFile = path.join(packagesDir, dir, "package.json");
+    writeJson(packageFile, {
+        ...readJson(packageFile), 
+        version: pkg.version,
+    });
+});
